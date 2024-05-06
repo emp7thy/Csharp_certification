ca9ab2d23496c7858c2efffa12a74ac7b62d17f6
Sergey <holden9q@gmail.com>
2024-05-06 16:56:54 +0300

    Добавил решение Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите начальное число (M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число (N): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результат функции Аккермана {M} до {N}: ");
+Console.WriteLine(AkermanFunctions(M, N));
+
+int AkermanFunctions(int M, int N)
+
+{
+  if (M == 0) return N + 1;
+  else if (N == 0) return AkermanFunctions(M - 1, 1);
+  else return AkermanFunctions(M - 1, AkermanFunctions(M, N - 1));
+}
